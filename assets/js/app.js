fd2e87b20e24ebf48c52c1c7ccbee4a39e6b71f0
AlinaD <alina.dorosh@gmail.com>
2022-11-14 11:47:11 +0100

    last commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,20 @@
 window.addEventListener("load", () => {
+  // JS Alina
+let dots = document.querySelectorAll(".fa-circle");
+let image = document.querySelector(".gallery-image");
+let galleryContainer = document.querySelector(".gallery-container");
+console.log(image);
+for (let i = 0; i < dots.length; i++) {
+  dots[i].addEventListener("click", () => {
+    image.src = "img/slider/" + (i + 1) + ".jpg";
+    let active = document.querySelector(".active");
+    active.classList.remove("active");
+    dots[i].classList.add("active");
+    galleryContainer.classList.toggle("flashit");
+    image.classList.toggle("flashit");
+  });
+}
+
   let iconMenu = document.body.querySelector(".icon-menu");
   let mainMenu = document.body.querySelector(".main-menu");
   let closeMenu = document.body.querySelector(".icon-close-menu");
@@ -124,18 +140,4 @@ window.addEventListener("load", () => {
   //Closing brace window load
 });
 
-// JS Alina
-let dots = document.querySelectorAll(".fa-circle");
-let image = document.querySelector(".gallery-image");
-let galleryContainer = document.querySelector(".gallery-container");
-console.log(image);
-for (let i = 0; i < dots.length; i++) {
-  dots[i].addEventListener("click", () => {
-    image.src = "img/slider/" + (i + 1) + ".jpg";
-    let active = document.querySelector(".active");
-    active.classList.remove("active");
-    dots[i].classList.add("active");
-    galleryContainer.classList.toggle("flashit");
-    image.classList.toggle("flashit");
-  });
-}
+
